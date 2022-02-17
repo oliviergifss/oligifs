59c10632b0fd295d094e5896aa29ff1817d753a7
oliviergifss <89310863+oliviergifss@users.noreply.github.com>
2022-02-17 20:11:34 +0100

    Update jason_bg.js

--- jason_bg.js
+++ jason_bg.js
@@ -1,8 +1,8 @@
 
 
 var background = [
-    'https://i.postimg.cc/q4NB2pS0/hola-bg-01.jpg',
-    'https://i.postimg.cc/q4NB2pS0/hola-bg-01.jpg',
-    'https://i.postimg.cc/q4NB2pS0/hola-bg-01.jpg',
-    'https://i.postimg.cc/q4NB2pS0/hola-bg-01.jpg'
+    'https://i.postimg.cc/KYsyZY3G/b-LBX3z-Hxd.jpg',
+    'https://i.postimg.cc/4xBD5356/W9-Eg-XGtxv-O0.jpg',
+    'https://i.postimg.cc/y8tPs4Yq/6-V1-Dch-IQy.jpg',
+    'https://i.postimg.cc/x1wKBvtn/0-WGNx-Bmvxe-M.jpg'
 ];
