4c85333bcbad046007ef0495114aaf1de98cd02a
oliviergifss <89310863+oliviergifss@users.noreply.github.com>
2022-02-26 15:12:36 +0100

    Delete jason_bg.js

--- jason_bg.js
+++
@@ -1,8 +0,0 @@
-
-
-var background = [
-    'https://i.postimg.cc/KYsyZY3G/b-LBX3z-Hxd.jpg',
-    'https://i.postimg.cc/4xBD5356/W9-Eg-XGtxv-O0.jpg',
-    'https://i.postimg.cc/y8tPs4Yq/6-V1-Dch-IQy.jpg',
-    'https://i.postimg.cc/x1wKBvtn/0-WGNx-Bmvxe-M.jpg'
-];
